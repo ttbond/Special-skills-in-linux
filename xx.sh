5056820bb50d36ad014c83a561c53c44e47e5ddf
ttbond <xu_tun@qq.com>
2018-10-24 15:10:51 +0800

    Update xx.sh

--- xx.sh
+++ xx.sh
@@ -1,2 +1,5 @@
 #split strings in awk
 awk '{split($1,sp,"r");print sp[2]}' dsb.bed
+
+#extract bed.file from vcf file ( only if the vcf file is regulated)
+awk '{split($1,sp1,"r");printf("%s\t%s\t",sp1[2],$2);split($8,sp1,";");split(sp1[2],sp2,"=");printf("%s\n",sp2[2]);}' tandemDUP.vcf >tandemDUP.bed
